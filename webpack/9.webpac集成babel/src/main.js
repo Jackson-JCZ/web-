13bd21309b9af7113c72428e45ef1431a69e71f2
jackson <313771069@qq.com>
2022-05-11 14:51:19 +0800

    新增webpack中引入babel实现降级js

---
+++ webpack/9.webpac集成babel/src/main.js
@@ -0,0 +1,30 @@
+// 1. yarn add jquery
+// 2. public/index.html - 10个li
+// 3. 入口处引入jquery
+import $ from 'jquery'
+// 4. 编写隔行变色的代码
+$("#myUL>li:odd").css('color', 'red')
+$("#myUL>li:even").css('color', 'green')
+
+// 5. 引入css文件
+import "./css/index.css"
+
+// 6. 引入less文件
+import "./less/index.less"
+
+// 7. 手动引入一个图片文件
+// webpack眼里万物皆模块
+import imgObj from './assets/1.gif'
+let theImg = document.createElement("img")
+theImg.src = imgObj
+document.body.appendChild(theImg)
+
+// 8. 引入字体图标样式文件
+import "./assets/fonts/iconfont.css"
+let theI = document.createElement("i")
+theI.className = "iconfont icon-qq"
+document.body.appendChild(theI)
+
+// 9. 书写高版本的js语法
+const fn = () => {console.log("我是一个箭头函数")}
+console.log(fn);
