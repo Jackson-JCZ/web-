2d0fc72776a2ecd7dfb7c14cb1f0934e4ef1646e
jackson <313771069@qq.com>
2022-05-19 17:30:26 +0800

    新增slot和缓存组件的案例

---
+++ Vue学习/component-demo/src/views/01_UseDynamic.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="comName='UserName'">账号密码填写</button>
+        <button @click="comName='UserInfo'">个人信息填写</button>
+        <p>显示注册组件-动态切换:</p>
+        <div style="border: 1px solid red;">
+            <component :is="comName"></component>
+        </div>
+    </div>
+</template>
+
+<script>
+import UserName from '../components/01/UserName'
+import UserInfo from '../components/01/UserInfo'
+export default {
+    data() {
+        return {
+            comName: "UserName"
+        }
+    },
+    components: {
+        UserName,
+        UserInfo
+    }
+}
+</script>
---
+++ Vue学习/component-demo/src/views/06_UseTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <MyTable :arr="list"></MyTable>
+    <MyTable :arr="list">
+        <!-- scope: {row: obj} -->
+       <template v-slot="scope">
+            <a :href="scope.row.headImgUrl">{{ scope.row.headImgUrl }}</a>
+       </template>
+    </MyTable>
+    <MyTable :arr="list">
+       <template v-slot="scope">
+            <img style="width: 100px;" :src="scope.row.headImgUrl" alt="">
+       </template>
+    </MyTable>
+  </div>
+</template>
+
+<script>
+import MyTable from "../components/06/MyTable";
+export default {
+  components: {
+    MyTable,
+  },
+  data() {
+    return {
+      list: [
+        {
+          name: "小传同学",
+          age: 18,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210303/603f2d2153241.jpg",
+        },
+        {
+          name: "小黑同学",
+          age: 25,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210304/6040b101a18ef.jpg",
+        },
+        {
+          name: "智慧同学",
+          age: 21,
+          headImgUrl:
+            "http://yun.itheima.com/Upload/./Images/20210302/603e0142e535f.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
