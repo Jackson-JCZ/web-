d1c0e6ccbf6f49a773e99f7a91f390b84ead7f73
jackson <313771069@qq.com>
2022-05-21 19:56:00 +0800

    定义页面路由

--- vue-music-demo/src/main.js
+++ vue-music-demo/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from '@/router'
 Vue.config.productionTip = false
 
+import {Tabbar, TabbarItem, NavBar, Col, Row} from 'vant'
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(NavBar)
+Vue.use(Col)
+Vue.use(Row)
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ vue-music-demo/src/views/Layout/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <van-nav-bar :title="activeTitle" fixed />
+        <div class="main">
+            <!-- 二级路由挂载点 -->
+            <router-view></router-view>
+        </div>
+        <van-tabbar route>
+            <van-tabbar-item replace to="/layout/home" icon="home-o"
+            >首页</van-tabbar-item>
+            <van-tabbar-item replace to="/layout/search" icon="search"
+            >搜索</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeTitle: this.$route.meta.title, //默认顶部导航显示的标题
+        };
+    },
+    //路由切换 - 侦听$route对象改变
+    watch: {
+        $route() {
+            this.activeTitle = this.$route.meta.title
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* 中间内容区域 - 容器样式(留好上下导航所占位置) */
+.main {
+  padding-top: 46px;
+  padding-bottom: 50px;
+}
+</style>
